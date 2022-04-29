9c486abbef59d275c18c4140fd93ae6f014f9237
Ian Okumu <okumu.otsembo@gmail.com>
2022-04-29 09:44:04 +0300

    Gradle: Added settings for dagger hilt

--- app/src/main/java/com/eeyan/travellycompose/ui/home/HomePage.kt
+++ app/src/main/java/com/eeyan/travellycompose/ui/home/HomePage.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.eeyan.travellycompose.R
